3175a4ce99ed70d5240dda7f597f35553bc220ee
FDS-Andrew <fds-andrew@pswrd123.freedom.local>
2023-06-27 17:20:31 +0800

    all logs are now stored in xml format added os detection added ip_list.txt for multiple uses

--- demo.sh
+++ demo.sh
@@ -1,25 +1,28 @@
 #!usr/bin/env bash
 
-
-#read mask
+# IP and mask of local machine
 ip=$(hostname -I)	# grab ip using hostname -I
-mask=$(ip a | grep $ip | awk '{print $2}')	# grab network mask using ip
-touch ping_log.gnmap
-nmap -sn $mask --min-parallelism 10000 -T5 --host-timeout 1ms -oG ping_log.gnmap &> /dev/null	# get ips that are responsive
-touch ip_list
-cat ping_log.gnmap | grep "Host:" | cut -d '(' -f 1 | cut -d ' ' -f 2 > ip_list	# parse the log into an ip list
-echo "Acquired active IPs."
+mask=$(ip a | grep $ip | awk '{print $2}')	## grab network mask using ip ex: "127.xxx.xxx.xxx/xx"
 
-## scan the ports of ip list
-echo "Probing for open ports."
-file="ip_list"
-rm port_log.gnmap 2> /dev/null
-touch port_log.gnmap
-while read -r line; do
-	nmap -sS $line -T5 -oG port_log.gnmap -vv --append-output &> /dev/null &	# probe ports in parallel
-done < $file
-wait
-touch port_list
-cat port_log.gnmap | grep "Ports:" > port_list	# parse port log into a list
-## further parsing of the port log
-echo "done"
+# Responsive IPs and MAC Addresses
+echo "Initiating ping scan..."
+nmap -sn $mask --min-parallelism 10000 -T5 --host-timeout 5ms -oX ping_log.xml &> /dev/null	## ping scan the network for working ipv4 and mac, results stored in xml
+echo "	Ping scan finished."
+
+# Network IPs
+xmllint --xpath '//host/address[@addrtype="ipv4"]/@addr' ping_log.xml | cut -d '"' -f 2 > ip_list.txt	## parse xml file for IP and store it in ip_list.txt
+
+# Ports and Services
+echo "Probing for open ports..."
+nmap -sS -iL ip_list.txt --min-parallelism 10000 -T5 -oX port_log.xml &> /dev/null ## port scan ip_list.txt, results stored in xml
+echo "	Ports and services acquired."
+
+# OS
+echo "Scanning for Operating systems..."
+nmap -O -iL ip_list.txt --min-parallelism 10000 -T5 -oX os_log.xml &> /dev/null
+echo "	OS scan done."
+
+echo "Done"
+
+
+
